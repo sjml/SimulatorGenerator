05d3a356588be4f493a9b27c2eb0fb11005d71dd
Shane Liesegang <shane@techie.net>
2014-01-10 20:30:54 -0800

    Organizing directory, comments, config, smarter checking.
    --HG--
    rename : badwords.json => data/badwords.json
    rename : bls_rawlist.txt => data/bls_rawlist.txt
    rename : bls_rawpage.html => data/bls_rawpage.html
    rename : titlecase/__init__.py => lib/titlecase/__init__.py
    rename : titlecase/__init__.pyc => lib/titlecase/__init__.pyc
    rename : titlecase/tests.py => lib/titlecase/tests.py

--- SimulatorGenerator.py
+++ SimulatorGenerator.py
@@ -1,41 +1,72 @@
 #!/usr/bin/env python
 
+# built-ins
 import sys
 import os
 import ConfigParser
-import requests
 from xml.dom import minidom
 import json
 import random
 import mimetypes
+import time
 import datetime
 import subprocess
 import re
 import traceback
+import pickle
 
-import twitter
+# site packages
+import requests
+import twitter 
+
+# local
+sys.path.insert(0, "./lib")
 import titlecase
 
+# global constants
+CONFIG_PATH = "config/config.ini"
+CREDENTIALS_PATH = "config/credentials.ini"
+TWITTERGC_PATH = "config/twitter_global_config.json"
+TWITTER_STATUS_LIMIT = 140
+
+# globals
+config = ConfigParser.ConfigParser()
 creds = ConfigParser.ConfigParser()
 twitterApi = None
+twitterGlobalConfig = None
 
 def setup():
     global creds
+    global config
     global twitterApi
+    global twitterGlobalConfig
 
     random.seed()
 
-    creds.read("credentials.ini")
+    config.read(CONFIG_PATH)
+    creds.read(CREDENTIALS_PATH)
 
-    consumer_key = creds.get("twitter", "consumerkey")
-    consumer_secret = creds.get("twitter", "consumersecret")
-    access_token = creds.get("twitter", "accesstoken")
-    access_token_secret = creds.get("twitter", "accesstokensecret")
+    consumer_key = creds.get("twitter", "consumer_key")
+    consumer_secret = creds.get("twitter", "consumer_secret")
+    access_token = creds.get("twitter", "access_token")
+    access_token_secret = creds.get("twitter", "access_token_secret")
     twitterApi = twitter.Api(consumer_key, consumer_secret, access_token, access_token_secret)
 
-def getJobTitle():
+    oneDayAgo = time.time() - 60*60*24
+    if (not os.path.exists(TWITTERGC_PATH) or os.path.getmtime(TWITTERGC_PATH) < oneDayAgo):
+        print("Getting configuration data from Twitter.")
+        twitterGlobalConfig = twitterApi.GetHelpConfiguration()
+        with open(TWITTERGC_PATH, "w") as tgcFile:
+            json.dump(twitterGlobalConfig, tgcFile)
+    else:
+        with open(TWITTERGC_PATH, "r") as tgcFile:
+            twitterGlobalConfig = json.load(tgcFile)
+    
+
+def getRandomJobTitle():
     # TODO: store off the category, don't repeat job titles, rotate categories
     #   http://api.careerbuilder.com/CategoryCodes.aspx
+    # TODO: Alternately, forget CareerBuilder and have this use BLS job titles.
 
     cb_apiKey = creds.get("careerbuilder", "apikey")
     js_params = {
@@ -45,15 +76,31 @@ def getJobTitle():
     }
     cb_URL = "http://api.careerbuilder.com/v1/jobsearch?"
 
-    response = requests.get(cb_URL, params=js_params)
-    dom = minidom.parseString(response.content)
-    # dom = minidom.parse(open("sample_jobsearch.xml"))
+    if (config.get("services", "careerbuilder_live") == 1):
+        response = requests.get(cb_URL, params=js_params)
+        dom = minidom.parseString(response.content)
+    else:
+        with open("offline-samples/sample_jobsearch.xml") as jobFile:
+            dom = minidom.parse(jobFile)
+
     jobs = []
     for node in dom.getElementsByTagName("JobTitle"):
         jobs.append(node.firstChild.nodeValue)
 
-    job = random.choice(jobs)
+    # NOTE: in the year 10,000 AD, this will need to be updated
+    maxLength = TWITTER_STATUS_LIMIT - (len(" Simulator ") + 4 + twitterGlobalConfig["characters_reserved_per_media"])
+    job = ""
+    count = 0
+    while (len(job) == 0 or len(job) > maxLength):
+        if (count >= 25):
+            # buncha really long job titles up in here
+            job = job[:maxLength-1] # (not great, but there are worse things)
+            break
+        job = random.choice(jobs)
+        count += 1
     job = job.replace("'", "\\'").replace('"', '\\"')
+
+    print("%i iteration(s) found random job title: %s" % (count, job))
     return job
 
 def getImageFor(searchTerm):
@@ -66,11 +113,16 @@ def getImageFor(searchTerm):
     headers = {"Referer" : "https://twitter.com/SimGenerator"}
     is_URL = "https://ajax.googleapis.com/ajax/services/search/images"
 
-    imageResults = requests.get(is_URL, params=is_params).json()
-    # imageResults = json.load(open("sample_imagesearch.json"))
+    if (config.get("services", "googleimage_live") == 1):
+        imageResults = requests.get(is_URL, params=is_params).json()
+    else:
+        with open("offline-samples/sample_imagesearch.json") as isFile:
+            imageResults = json.load(isFile)
+
     if (imageResults == None or 'responseData' not in imageResults or imageResults['responseData'] == None):
         sys.stderr.write("No response data in image search for %s. JSON:\n%s\n" % (searchTerm, imageResults))
         return
+
     imageData = []
     for image in imageResults['responseData']['results']:
         imageData.append(
@@ -102,10 +154,9 @@ def getImageFor(searchTerm):
         if (extension == ".jpe") : extension = ".jpg"
 
         localFileName = "tmp/base_image-%s%s" % (datetime.datetime.now().strftime("%Y-%m-%d-%H%M.%f"), extension)
-        baseFile = open(localFileName, 'wb')
         imgResponse = requests.get(img['url'])
-        baseFile.write(imgResponse.content)
-        baseFile.close()
+        with open(localFileName, 'wb') as baseFile:
+            baseFile.write(imgResponse.content)
 
         # check our work
         cmdLine = ['identify', '-format', '%wx%h', localFileName]
@@ -172,33 +223,42 @@ def createBoxArt(jobTitle, localImgFile, year):
         widthMultiplier = 0.95
 
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d-%H%M.%f")
+    outputFile = "tmp/output-%s.png" % timestamp
     offset = "+%i+%i" % (cap(dimensions[0] * .05, 20), cap(dimensions[1] * .05, 20))
     command = [
         "convert",
-        "-background", "none",
+
+        # generate large text that we'll scale down to fit the target image later
+        "-background", "none", 
         "-fill", "white",
         "-stroke", "gray",
         "-strokewidth", "3",
         "-kerning", "-5",
-        "-font", "./helvetica-ultra-compressed.ttf",
+        "-font", "./data/helvetica-ultra-compressed.ttf",
         "-pointsize", "300",
         "-gravity", align,
         "-interline-spacing", "75",
         ("label:%s" % jobTitle).encode("utf8"),
-        "-shear", "10x0",
-        "-trim",
+        "-shear", "10x0", # since this font doesn't have true oblique / italics
+        "-trim", # remove the extra space added by the newline wrapping
+
+        # bring its size down so it can be overlaid into the base image
         "-resize", "%ix%i" % (dimensions[0] * widthMultiplier, dimensions[1] * .95),
-        localImgFile, "+swap",
+
+        localImgFile, "+swap", # take everything we just created and make it
+                               #  the thing that we're overlaying. localImgFile
+                               #  becomes the target
         "-gravity", grav,
         "-geometry", offset,
         "-composite",
-        "-resize", "1500x1500>",
-        "output-%s.png" % timestamp
+        "-resize", "%ix%i>" % (twitterGlobalConfig["photo_sizes"]["large"]["w"], twitterGlobalConfig["photo_sizes"]["large"]["h"]), 
+        outputFile
     ]
 
+    print("ImageMagick command: %s", " ".join(command))
     subprocess.call(command)
     os.rename(localImgFile, "archive/%s" % os.path.basename(localImgFile))
-    return "output-%s.png" % timestamp
+    return outputFile
 
 def tweet(job, year, artFile, respondingTo=None):
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d-%H%M.%f")
@@ -213,18 +273,19 @@ def tweet(job, year, artFile, respondingTo=None):
     if (artFile != None and os.path.exists(artFile)):
         if (userName != None):
             title = "@%s %s" % (userName, title)
-        twitterApi.PostMedia(title, artFile, in_reply_to_status_id=requestId)
+        if (config.get("services", "twitter_live") == 1):
+            twitterApi.PostMedia(title, artFile, in_reply_to_status_id=requestId)
+        else:
+            print("Would have posted '%s' to twitter with image: %s" % (title, artFile))
 
         os.rename(artFile, "archive/image-%s.png" % timestamp)
-        archFile = open("archive/text-%s.txt" % timestamp, "w")
-        archFile.write(title.encode('utf8'))
-        archFile.close()
+        with open("archive/text-%s.txt" % timestamp, "w") as archFile:
+            archFile.write(title.encode('utf8'))
     else:
         # don't tweet; something's wrong. 
         print("FAILURE: %s" % title.encode("utf8"))
-        archFile = open("archive/failed-%s.txt" % timestamp, "w")
-        archFile.write(title.encode('utf8'))
-        archFile.close()
+        with open("archive/failed-%s.txt" % timestamp, "w") as archFile:
+            archFile.write(title.encode('utf8'))
 
 def checkTwitterLimits():
     rateLimitData = twitterApi.GetRateLimitStatus()
@@ -236,29 +297,46 @@ def checkTwitterLimits():
 
     print mentionsCallsLeft, "mentions calls left."
 
+
+def manualJobTweet(job, year=None):
+    job = titlecase.titlecase(job)
+    image = getImageFor(job)
+    if (year == None):
+        year = random.randomint(2007, datetime.date.today().year)
+    art = createBoxArt(job, image, year)
+    tweet(job, year, art)
+
 def randomJobTweet():
-    job = getJobTitle()
+    job = getRandomJobTitle()
     image = getImageFor(job)
     year = random.randint(2007, datetime.date.today().year)
     art = createBoxArt(job, image, year)
     tweet(job, year, art)
 
 def respondToRequests():
+    if (config.get("settings", "taking_requests") == 0):
+        print("Not taking requests.")
+        return
+
     lastReply = 0
     lastReplyFile = "last_replied_to.txt"
     if (os.path.exists(lastReplyFile)):
-        with open(lastReplyFile, "r") as f:
-            lastReply = int(f.read())
+        with open(lastReplyFile, "r") as lpf:
+            lastReply = int(lpf.read())
 
-    badwordsFile = open("badwords.json", "r")
-    badwordsData = json.load(badwordsFile)
-    badwordsFile.close()
+    with open("data/badwords.json", "r") as badwordsFile:
+        badwordsData = json.load(badwordsFile)
     badwords = badwordsData['badwords']
 
     requestRegex = re.compile('make one about ([^,\.\n@]*)', re.IGNORECASE)
 
-    mentions = twitterApi.GetMentions(since_id=lastReply)
-    mentions.reverse()
+    if (config.get("services", "twitter_live") == 1):
+        mentions = twitterApi.GetMentions(since_id=lastReply)
+    else:
+        with open("offline-samples/twitter-mentions.pickle", "rb") as mentionArchive:
+            mentions = pickle.load(mentionArchive)
+
+    mentions.reverse() # look at the newest one last and hold our place there
     for status in mentions:
         result = requestRegex.search(status.text)
         if (result):
@@ -270,15 +348,28 @@ def respondToRequests():
                 job = job[3:]
             job = titlecase.titlecase(job)
 
+            year = random.randint(2007, datetime.date.today().year)
+
             earlyOut = False
+            # check for derogatory speech
             for word in job.split():
                 if word in badwords:
                     earlyOut = True
-            if earlyOut: continue
+                    break
+            # see if we'll even be able to post back at them
+            if len("@%s %s Simulator %i" % 
+                    (status.user.screen_name, 
+                     job, 
+                     year)
+                    ) + twitterGlobalConfig["characters_reserved_per_media"] > TWITTER_STATUS_LIMIT:
+                earlyOut = True
+
+            if earlyOut:
+                # TODO: consider tweeting back "no" at them? 
+                continue
 
             try:
                 image = getImageFor(job)
-                year = random.randint(2007, datetime.date.today().year)
                 art = createBoxArt(job, image, year)
                 tweet( job, year, art, (status.user.screen_name, str(status.id)) )
             except Exception, e:
