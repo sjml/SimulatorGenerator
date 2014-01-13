543d6e24f78c0c8752f73c21b1bf16c9408c298f
Shane Liesegang <shane@techie.net>
2014-01-13 11:15:26 -0800

    Basic pass at normalized job list

--- data/BLS_processor.py
+++ data/BLS_processor.py
@@ -1,26 +1,12 @@
 import os
 import sys
+import re
 from collections import OrderedDict
 
 with open("bls_rawlist.txt", "r") as jobFile:
     rawJobs = map(str.rstrip, jobFile.readlines())
     rawJobsUnique = OrderedDict.fromkeys(rawJobs).keys()
 
-def analyze():
-    commacount = 0
-    flscount = 0
-    inccount = 0
-    for job in rawJobsUnique:
-        if ", " in job:
-            commacount += 1
-        if "First-Line Supervisors" in job:
-            flscount += 1
-        if "Including" in job:
-            inccount += 1
-
-    print "%i jobs with commas." % (commacount)
-    print "%i jobs with fls." % (flscount)
-    print "%i jobs with including." % (inccount)
 
 def findInList(needle, haystack):
     count = 0
@@ -35,12 +21,14 @@ lastMods = {}
 for job in rawJobsUnique:
     # split on commas
     jobSplit = map(str.strip, job.split(","))
-    if len(jobSplit) == 1:
-        jobs.append(job)
-        continue
 
     # <sigh> there are typos in this list
     jobSplit = filter(lambda x: len(x) > 0, jobSplit)
+
+    # no commas! this one is easy
+    if len(jobSplit) == 1:
+        jobs.append(job)
+        continue
 
     # if any elements have "except", remove them and everything after
     index = findInList("Except", jobSplit)
@@ -100,13 +88,25 @@ for job in rawJobsUnique:
         jobs.append(jobString)
         continue
 
-    materials = ("Metal and Plastic", "Synthetic and Glass Fibers", "Wood")
+    materials = (
+        "Metal and Plastic", 
+        "Synthetic and Glass Fibers", 
+        "Wood", 
+        "Plastics", 
+        "Food and Tobacco"
+    )
     matIndex = findInList(jobSplit[-1], materials)
     if (matIndex != None):
+        jobSplit = map(lambda x: re.sub("Operator$", "Operators", x), jobSplit)
         material = materials[matIndex]
         setIndex = findInList("Setters", jobSplit)
         opIndex = findInList("Operators", jobSplit)
         tendIndex = findInList("and Tenders", jobSplit)
+        if (opIndex != None and opIndex == tendIndex):
+            jobSplit.insert(0, jobSplit.pop(-1))
+            jobString = ', '.join(jobSplit)
+            jobs.append(jobString)
+            continue
         if (setIndex != None and opIndex != None and tendIndex != None):
             jobSplit[setIndex] = jobSplit[setIndex][:-len(" Setters")]
             jobSplit.pop(tendIndex)
@@ -181,65 +181,69 @@ for job in rawJobsUnique:
 
 
     if (jobSplit[-1].startswith("and ")):
-        print jobSplit
-        titles = []
-        modifiers = []
-        lastTitle = jobSplit[-1].split()[-1]
-        jobSplit[-1] = ' '.join(jobSplit[-1].split()[:-1])
-        # print lastTitle, jobSplit
-        # print jobSplit
-        # titles.append(lastTitle)
-        # modifiers.append(jobSplit[-1][len("and "):].split()[0])
-        # print modifiers, jobSplit
+        jobString = ', '.join(jobSplit)
+        jobs.append(jobString)
+        continue
 
-    # print jobSplit
-    if (jobSplit[0] not in lastMods):
-        lastMods[jobSplit[0]] = 0
-    lastMods[jobSplit[0]] += 1
+    if jobSplit[0] == "Sales Representatives":
+        jobSplit.pop(0)
+        jobSplit.insert(0, "Sales")
+        jobSplit.insert(0, "Representatives")
+
+    if jobSplit[0] == "Representatives":
+        selling = []
+        if len(jobSplit) > 3:
+            selling = jobSplit[3:]
+            jobSplit = jobSplit[:3]
+        jobSplit.reverse()
+        if len(selling) > 0:
+            jobSplit.append("for")
+            jobSplit.append(' '.join(selling))
+        jobString = ' '.join(jobSplit)
+        jobs.append(jobString)
+        continue
+
+    # hmmm, this feels someone defending a very specific job...
+    if jobSplit[-1].endswith("Enhanced Operators/Maintainers"):
+        jobSplit[-1] = jobSplit[-1][:-len(" Enhanced Operators/Maintainers")]
+        jobString = "%s for %s" % (
+            "Enhanced Operators/Maintainers",
+            ', '.join(jobSplit)
+        )
+        jobs.append(jobString)
+        continue
+
+    # if there's still an "and" in the last element, assume an Oxford comma was dropped
+    #  and pick it up
+    if " and " in jobSplit[-1]:
+        last = jobSplit.pop(-1)
+        jobSplit = jobSplit + map(str.strip, last.split(" and "))
+        for j in jobSplit:
+            jobs.append(j)
+        continue
+
+    # if there's an "and" in the penultimate slot, the last word is a modifier
+    if jobSplit[-2].startswith("and "):
+        mod = jobSplit.pop(-1)
+        jobSplit[-1] = jobSplit[-1][len("and "):]
+        firstSplit = jobSplit[0].split()
+        if len(firstSplit) > 1:
+            mod += " " + firstSplit[0]
+            jobSplit[0] = ' '.join(firstSplit[1:])
+        for j in jobSplit:
+            jobString = "%s %s" % (mod, j)
+            jobs.append(jobString)
+        continue
+
+    # finally down to some simple inversions
+    jobSplit.reverse()
+    jobString = ' '.join(jobSplit)
+    jobs.append(jobString)
 
 
-# import pprint
-# pprint.pprint(lastMods)
+# unique-ify
+jobs = list(set(jobs))
+jobs.sort()
 
-## invert
-# Accountants, Certified Public
-
-## split out
-# Actors, Producers, and Directors
-
-## remove post except
-# Administrative Assistants, Except Legal, Medical, and Executive
-
-## remove All Other
-# Administrative Support Workers, All Other
-
-## remove FLS
-# Administrative Support Workers, First-Line Supervisors
-
-## just to make sure this double-and doesn't trip us up
-# Agents and Business Managers of Artists, Performers, and Athletes
-
-## remove except, THEN invert
-# Agents, Purchasing, Except Wholesale, Retail, and Farm Products
-
-## separate categories?
-# Artists, Fine, Including Painters, Sculptors, and Illustrators
-
-## wtf why -- ah, simple invert
-# Boring Machine Tool Operators, Metal and Plastic
-# Buffing Machine Tool Operators, Metal and Plastic
-# Buffing Machine Tool Setters, Metal and Plastic
-# Buffing Machine Tool Tenders, Metal and Plastic
-
-### got down to Digitizers, Apparel Embroidery
-
-
-# Handlers, Pesticide, Vegetation
-# Drivers, Bus, Intercity
-# Drivers, Bus, School or Special Client
-# Drivers, Bus, Transit
-
-# Repairers, Electronics, Powerhouse
-
-# Representatives, Sales, Wholesale and Manufacturing, Except Technical and Scientific Products
-# Representatives, Sales, Wholesale and Manufacturing, Technical and Scientific Products
+with open("bls_normalized.txt", "w") as normalizedFile:
+    normalizedFile.write('\n'.join(jobs))
